da86eedaa24ec745480f5ebcd84348636f244370
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-05-13 15:53:09 -0300

    windows

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,11 +3,13 @@ import Products from "./Componnents/Products";
 import Product from "./Componnents/Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./ComponnentGlobal/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Products />} />
           <Route path="product/:id" element={<Product />} />
