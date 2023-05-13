f8bbc16df9943467e57ae6e7eb200df1fe550829
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-05-13 15:43:59 -0300

    page unitary product

--- ecommerce/src/Componnents/Product.jsx
+++ ecommerce/src/Componnents/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Product.module.css";
 import { useParams } from "react-router-dom";
 
 const Product = () => {
@@ -15,14 +16,34 @@ const Product = () => {
     }
 
     restAPI();
-  }, []);
+  }, [id]);
 
   if (data === null) return null;
   console.log(data);
 
   return (
     <div>
-      <p>{data.produto}</p>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <img src={data.image} alt="Imagem telefone" />
+        </div>
+        <div className={styles.right}>
+          <h1>{data.produto}</h1>
+          <h2>Confira esse preço imbátivel</h2>
+          <p>
+            <span>R$</span>
+            {data.preço}
+          </p>
+          <div className={styles.about}>
+            <p>Sobre este item: </p>
+            {data.descricao.map((item) => (
+              <ul>
+                <li key={item}>{item}</li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
